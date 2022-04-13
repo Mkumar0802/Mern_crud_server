42586d7e7fca90babeac5e23f7f3dff6d33b4a36
mkumar0802 <mkumar0802@gmail.com>
2022-04-13 10:49:21 +0530

    make it better

--- Module/employeemodule.js
+++ Module/employeemodule.js
@@ -1,58 +1,62 @@
 const Employee = require('../Model/Employee')
 const Joi = require('joi');
 
-///////// create movie ///////////////////
-exports.postemployee = async (req,res,next)=>{
+exports.postemployee = async (req, res, next) => {
     //joi validate schema
-        const schema = Joi.object({
-            name: Joi.string().min(1).max(30).required(),
-            photo: Joi.string().required(),
-            price: Joi.string().required(),
-            details: Joi.string().required(),
+    const schema = Joi.object({
+        name: Joi.string().min(1).max(30).required(),
+        age: Joi.string().required(),
+        country: Joi.string().required(),
+        email: Joi.string().required(),
+        mobile: Joi.string().required(),
+        
+
     })
     // joi validate input data
-    var {error} = await schema.validate(req.body);
-    if(error){
-        return res.status(400).send({msg:error.details[0].message})
+    var { error } = await schema.validate(req.body);
+    if (error) {
+        return res.status(400).send({ msg: error.details[0].message })
     }
     res.send('success')
-    
+
     ////////////////// save data in mongodb using mongoose //////////////
-    const employee = new Employee ({ 
+    const employee = new Employee({
         name: req.body.name,
-        photo: req.body.photo,
-        price: req.body.price,
-        details: req.body.details, 
+        age: req.body.age,
+        country: req.body.country,
+        email: req.body.email,
+        mobile: req.body.mobile,
+        
     })
-    try{
-    var response=await employee.save();  
-    res.send(response) 
-    } catch(err){
-    res.status(400).send(err)
+    try {
+        var response = await employee.save();
+        res.send(response)
+    } catch (err) {
+        res.status(400).send(err)
     }
 }
 
-///////////////// get movie //////////////////
-exports.getemployee = async (req,res,next)=>{
+//////////////////////// get  ///////////////////
+exports.getemployee = async (req, res, next) => {
     const response = await Employee.find();
     res.send(response);
 }
- 
-////////// update theatre name & address ///////////////
-exports.updateemployee = async (req,res,next)=>{
-    const {employeeId} = req.params;   // object destructure
-    const response = await Employee.findByIdAndUpdate(employeeId,{
+
+exports.updateemployee = async (req, res, next) => {
+    const { employeeId } = req.params;   // object destructure
+    const response = await Employee.findByIdAndUpdate(employeeId, {
         name: req.body.name,
-        photo: req.body.photo,
-        price: req.body.price,
-        details: req.body.details,
+        age: req.body.age,
+        country: req.body.country,
+        email: req.body.email,
+        mobile: req.body.mobile
     });
-    res.send(response);  
+    res.send(response);
 }
 
-////////// delete theatre ///////////////
-exports.deleteemployee = async (req,res,next)=>{  
-    const {employeeId} = req.params;   // object destructure
+////////// delete  /////////////// 
+exports.deleteemployee = async (req, res, next) => {
+    const { employeeId } = req.params;   // object destructure 
     const response = await Employee.findByIdAndRemove(employeeId)
     res.send(response);
 }
